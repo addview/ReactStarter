a43b0c07a52ccbae6558d6233d79c1ed8709651b
addview <fredrik@addview.se>
2015-09-29 16:31:42 +0200

    fixat
    fixat sista

--- src/components/comment-box.jsx
+++ src/components/comment-box.jsx
@@ -8,12 +8,13 @@ module.exports = React.createClass({
 		
 	},
 	renderComments: function(){
+		
 		return this.props.comments.slice(0,20).map(function(comment){
 			return <li className="list-group-item comment-box" key={comment.id}>
-				<span className="badge">{comment.up}</span>
+				<span className="badge">{comment.ups}</span>
 				<h5>{comment.author}</h5>
 				{comment.comment}
-			</li>
+			</li>			
 		})
 	}
 });
